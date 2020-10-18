cffe8c8deb0bb9513b3b0fc894c120651ea4a210
icapetti <icapetti@hotmail.com>
2020-10-18 19:25:32 -0300

    Create extract_pdf_convert_csv.py
    Extract a pdf file from internet and convert to csv.

---
+++ extract_pdf_convert_csv.py
@@ -0,0 +1,37 @@
+# Install tabula and requests: pip install tabula-py and pip install requests
+# Imports
+import requests as req
+import tabula as tb
+import pandas as pd
+
+# Function to download pdf file
+def download_file(url, file_path):
+    response = req.get(url)
+    if response.status_code == req.codes.OK:
+        with open(file_path, 'wb') as new_file:
+                new_file.write(response.content)
+        print("Download is finished. File saved: {}".format(file_path))
+    else:
+        response.raise_for_status()
+
+
+def main():
+    # Link dos dados se auxilio de imoveis para os senadores:
+    # https://www12.senado.leg.br/transparencia/sen/auxilio-moradia-e-imoveis-funcionais
+    # http://www.senado.gov.br/transparencia/lai/secrh/senador_auxilio_imoveis_pdf.pdf
+
+    pdf_file = "pdf_file.pdf"
+    csv_file = "csv_file.csv"
+    
+    url = input('URL: ')
+    path = input('PATH: ')
+
+    download_file(url, path+pdf_file)
+
+    try:
+        tb.convert_into(path+pdf_file, path+csv_file, output_format="csv", pages="all")
+        print("PDF converted and CSV saved.")
+    except:
+        print("Something went wrong.")
+
+main()
